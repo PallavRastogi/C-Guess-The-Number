a437e51fb459b02c5bb1d7e656b4657641447da7
pallav1650 <92592779+pallav1650@users.noreply.github.com>
2021-10-15 20:48:02 +0530

    Update Guess The Number.cpp

--- Guess The Number.cpp
+++ Guess The Number.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 using namespace std;
 
-
 int main(){
     cout<<"-- Welcome To My Game --\n";
     cout<<"RULES -\n#1) You Have to Guess a Number Between 1-100.\n#2) You Will Get 7 Chances/guesses.\n#3) After Every Guess You Will Get a Feedback Indicating Whether You Guessed Too High Or Too Low.\n#4) Good Luck! :D\n\n";
-    int secret = 69;
+    int secret = 34;
     int ui;
     bool exit_number = 0;
     
     for(int g = 1; g < 8; g++){
-        cout<<"------------------\nGuess #"<<g<<"-\nEnter Your Guess:\n";
+        cout<<"------------------\nGuess #"<<g<<"-\nEnter Your Guess: \n";
         cin>>ui;
         cout<<"\n";
         
@@ -34,4 +33,9 @@ int main(){
     
     return 0;
 }
+
+
+// can add random number functionality
+// same number each time prog is run
+// compile to exe by "g++ <filename.cpp>"
 //end of file//
